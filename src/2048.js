7d4d0bd0e900e0d524935b927a6c66d61923646f
koziscool <kozjob@yahoo.com>
2016-04-11 17:21:18 -0500

    best so far

--- src/2048.js
+++ src/2048.js
@@ -84,15 +84,16 @@ var Board2048 = React.createClass({
 
 var Tile2048 = React.createClass({
   render: function() {
-    var colors = ['red', 'gray', 'blue'];
+    var colors = [ 'lightgray','orange', 'lightgreen',  'red', 'gray', 'blue', 'purple', 'brown', 'black'];
     var row = Math.floor( this.props.index / FOUR);
     var col = this.props.index % FOUR;
+    var colorIndex = Math.max( Math.floor(Math.log2( this.props.value )), 0 );
     var style = {
       left: col * 128,
       top: row * 128,
-      background: colors[this.props.index % colors.length]
-    };
+      background: colors[colorIndex],
 
+    };
     return (
       <div className="animateItem" style={style}>{this.props.value}</div>
     );
